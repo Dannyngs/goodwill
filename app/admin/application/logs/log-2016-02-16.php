7c993731f892cff486deecdd67c7de88198d5103
dannyng198811 <danny.ng198811@yahoo.com>
2016-02-16 13:39:59 +0800

    d

---
+++ app/admin/application/logs/log-2016-02-16.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-02-16 05:39:11 --> Severity: Notice  --> Only variable references should be returned by reference E:\goodwill\app\admin\system\core\Common.php 257
+ERROR - 2016-02-16 05:39:11 --> Severity: Notice  --> Only variable references should be returned by reference E:\goodwill\app\admin\system\core\Common.php 257
