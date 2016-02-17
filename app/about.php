972ea80be542bfeac880f83b312791ad1b044ef6
dannyng198811 <danny.ng198811@yahoo.com>
2016-02-17 15:29:37 +0800

    asd

--- app/about.php
+++ app/about.php
@@ -8,8 +8,40 @@ require_once 'header.php';
    <div class="content">
     <div class="container bgfff">
 
-     <?php include_once "banner.php" ;?>
+ <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+  </ol>
 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+   
+
+    <div class="item active">
+      <img src="_/images/banner1.jpg" alt="...">
+    </div>
+
+     <div class="item">
+      <img src="_/images/banner2.jpg" alt="...">
+    </div>
+
+     
+   
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+
+</div>
 <div class="row">
   <div class="col-md-12 mt15 plr20">
     <h3 class="tit-yellow"><?php echo $lang['intro'];?>:</h3>
